c1ffb8f9b6485a2f7b8b26ce47f97ea2487b7a29
Alexey <pwz-and_10@mail.ru>
2022-10-01 15:48:11 +0600

    Create Task1

---
+++ Task1.py
@@ -0,0 +1,18 @@
+def Input(inText):
+    is_OK = False
+    while not is_OK:
+        numb = float(input(f"{inText}"))
+        is_OK = True
+        return numb
+
+
+def Summ(number):
+    sum = 0
+    for i in str(number):
+        if i != ".":
+            sum += int(i)
+    return sum
+
+
+number = Input("Введите число: ")
+print(f"Результат суммы цифр = {Summ(number)}")
